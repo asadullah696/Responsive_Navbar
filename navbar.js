ca74d69eee62f25592166ed0cde44020772c8eaa
asadullah696 <asadullahrajpoot19@gmail.com>
2023-09-03 15:26:16 +0500

    JavaScript file for the main page of Responsive Navbar page

---
+++ navbar.js
@@ -0,0 +1,10 @@
+function menuButton(){
+    let navlist = document.getElementById("navol2").style.display = "block";
+    let cutMenu = document.getElementById("cutMenu").style.display = "flex";
+    let menu = document.getElementById("menu").style.display = "none"
+}
+function hideCross(){
+    let hideCross = document.getElementById("cutMenu").style.display = "none"
+    let menu = document.getElementById("menu").style.display = "flex"
+    let navlist = document.getElementById("navol2").style.display = "none";
+}
